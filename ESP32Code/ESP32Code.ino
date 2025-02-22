96ba6fba3bd095d3b41f030100a9ad77768bb681
dan71853 <dan71853@gmail.com>
2025-02-23 11:28:52 +1300

    - Added some comments - Renamed some variables

--- ESP32Code/ESP32Code.ino
+++ ESP32Code/ESP32Code.ino
@@ -4,25 +4,26 @@ const uint8_t POWER_POT = 39;   // Potentiometer to control how long relay/solen
 const uint8_t ARM_BTN = 32;     // Momentary illuminated push button to arm/disarm, pull up pin internally
 const uint8_t STATUS_LED = 33;  // LED in the push button (can be powered directly from pin)
 
-const uint8_t LIMIT_SW = 27; //Limit switch to detect mortar shell
-const uint8_t RELAY = 2;  // 14
+const uint8_t LIMIT_SW = 27;     // Limit switch to detect mortar shell
+const uint8_t SOLENOID_PIN = 2;  // 14 set to onboard led (2) for testing //Relay or MOSFET to control solenoid
 
-const bool RELAY_ON_STATE = HIGH;     // Relay state to fire motor
-const bool LIMIT_SWITCH_FIRED = LOW;  // State of limit switch when it is triggered
+// Define ON/OFF state for inputs/outputs
+const bool SOLENOID_ON_STATE = HIGH;      // true = Activate solenoid
+const bool LIMIT_SWITCH_ACTIVATED = LOW;  // true = Mortar shell detected by limit switch
 
 // Callbacks for external interrupts
 void IRAM_ATTR armBtnCb();
-void IRAM_ATTR limitSwCb();
 
-// Global variables
-uint16_t triggeredTimeThreshold = 1000;
-const uint16_t TRIGGERED_TIME_MIN = 100;
-const uint16_t TRIGGERED_TIME_MAX = 1000;
+// Global variables for timing, time in ms
+uint16_t triggerDelayThreshold = 1000;  // Initial delay
+const uint16_t TRIGGER_DELAY_MIN = 100;
+const uint16_t TRIGGER_DELAY_MAX = 1000;
 
-uint16_t fireTimeThreshold = 1000;
+uint16_t fireTimeThreshold = 1000;  // Time solenoid is open for
 const uint16_t FIRE_TIME_MIN = 100;
 const uint16_t FIRE_TIME_MAX = 1000;
 
+// State machine states
 enum States {
   DISARMED,
   ARMED,
@@ -30,7 +31,9 @@ enum States {
   FIRING,
   PRELOAD
 };
+volatile enum States currentState = DISARMED;
 
+// States in string format for printing
 char *stateNames[] = {
   "DISARMED",
   "ARMED",
@@ -38,62 +41,63 @@ char *stateNames[] = {
   "FIRING",
   "PRELOAD",
 };
-volatile enum States currentState = DISARMED;
-
-// Delays
-uint16_t initialDelay = 0;
-uint16_t powerDelay = 0;
 
 void setup() {
   Serial.begin(115200);
 
-  // Setup relay output
-  pinMode(RELAY, OUTPUT);
-  digitalWrite(RELAY, !RELAY_ON_STATE);
+  // Setup solenoid output and turn it off
+  pinMode(SOLENOID_PIN, OUTPUT);
+  digitalWrite(SOLENOID_PIN, !SOLENOID_ON_STATE);
 
   // Setup arm button with interrupt
   pinMode(ARM_BTN, INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(ARM_BTN), armBtnCb, RISING);
 
-  // Setup limit switch with interrupt
+  // Setup limit switch, no interrupt nedded
   pinMode(LIMIT_SW, INPUT_PULLUP);
 
   // Setup Armed LED
   pinMode(STATUS_LED, OUTPUT);
   digitalWrite(STATUS_LED, LOW);
 
-  // Setup analog inputs
+  // Setup analog inputs, set the resoloution to 10Bits (0-1024)
   analogReadResolution(10);
   pinMode(DELAY_POT, INPUT);
   pinMode(POWER_POT, INPUT);
 }
 
 uint32_t armDebounceTimer = millis();
+const uint16_t debounceThreshold = 700;
+/**
+ * Callback for the rising edge of the pushbutton
+ * Debouncing time based of debounceThreshold
+ */
 void IRAM_ATTR armBtnCb() {
-  if (millis() - armDebounceTimer > 500) {
-    if (currentState == DISARMED)
-      currentState = ARMED;
-    else
-      currentState = DISARMED;
+  if (millis() - armDebounceTimer > debounceThreshold) {
+    // Arm button will override all other states
+    currentState = (currentState == DISARMED) ? ARMED : DISARMED;
     armDebounceTimer = millis();
   }
 }
 
-enum States lastPrintState;
 enum States lastState;
 uint32_t triggeredTimer = millis();
 uint32_t fireTimer = millis();
 void loop() {
 
+  if (lastState != currentState) {
+    Serial.printf("State: %s\n", stateNames[currentState]);
+  }
+
   switch (currentState) {
     case DISARMED:
-      // Run once
       if (lastState != currentState) {
-        digitalWrite(RELAY, !RELAY_ON_STATE);
+        // Run this part only once
         lastState = currentState;
       }
+      digitalWrite(SOLENOID_PIN, !SOLENOID_ON_STATE);
 
-      if (digitalRead(LIMIT_SW) == LIMIT_SWITCH_FIRED) {
+      if (digitalRead(LIMIT_SW) == LIMIT_SWITCH_ACTIVATED) {
         currentState = PRELOAD;
         break;
       }
@@ -102,8 +106,9 @@ void loop() {
 
     case ARMED:
       if (lastState != currentState) {
+        lastState = currentState;
       }
-      if (digitalRead(LIMIT_SW) == LIMIT_SWITCH_FIRED) {
+      if (digitalRead(LIMIT_SW) == LIMIT_SWITCH_ACTIVATED) {
         currentState = TRIGGERED;
         break;
       }
@@ -115,10 +120,10 @@ void loop() {
         lastState = currentState;
         triggeredTimer = millis();
         uint16_t potValue = analogRead(DELAY_POT);
-        triggeredTimeThreshold = map(potValue, 0, 1023, TRIGGERED_TIME_MIN, TRIGGERED_TIME_MAX);
-        Serial.printf("pot: %d, time: %d\n", potValue, triggeredTimeThreshold);
+        triggerDelayThreshold = map(potValue, 0, 1023, TRIGGER_DELAY_MIN, TRIGGER_DELAY_MAX);
+        Serial.printf("pot: %d, time: %d\n", potValue, triggerDelayThreshold);
       }
-      if (millis() - triggeredTimer > triggeredTimeThreshold) {
+      if (millis() - triggeredTimer > triggerDelayThreshold) {
         currentState = FIRING;
       }
       blinkLEDFast();
@@ -130,7 +135,7 @@ void loop() {
         fireTimer = millis();
         uint16_t potValue = analogRead(POWER_POT);
         fireTimeThreshold = map(potValue, 0, 1023, FIRE_TIME_MIN, FIRE_TIME_MAX);
-        digitalWrite(RELAY, RELAY_ON_STATE);
+        digitalWrite(SOLENOID_PIN, SOLENOID_ON_STATE);
         Serial.printf("pot: %d, time: %d\n", potValue, fireTimeThreshold);
       }
       if (millis() - fireTimer > fireTimeThreshold) {
@@ -144,18 +149,13 @@ void loop() {
         lastState = currentState;
       }
       blinkLEDFast();
-      if (digitalRead(LIMIT_SW) != LIMIT_SWITCH_FIRED) {
+      if (digitalRead(LIMIT_SW) != LIMIT_SWITCH_ACTIVATED) {
         currentState = DISARMED;
       }
       break;
 
     default:
       break;
-  }
-
-  if (lastPrintState != currentState) {
-    Serial.printf("State: %s\n", stateNames[currentState]);
-    lastPrintState = currentState;
   }
 }
 
